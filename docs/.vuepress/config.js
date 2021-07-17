7d4bac3eacebe4199ce89ebedcdc9ac35b7238b4
xiaofuyesnew <xiaofuyesnew@qq.com>
2021-07-18 00:21:44 +0800

    upload

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,9 @@
 module.exports = {
   bundler: '@vuepress/vite',
   title: '机枢',
-  description: '一个低代码平台'
+  description: '一个低代码平台',
+  themeConfig: {
+    logo: '/images/logo.jpg',
+    // logo: 'https://vuejs.org/images/logo.png'
+  },
 }
